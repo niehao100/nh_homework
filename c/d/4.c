be3c5b8c8a58c8222dea4648d6a024a6ac274340
nh13 <niehao100@gmail.com>
2014-11-11 14:27:41 +0800

    BTree

--- c/d/4.c
+++ c/d/4.c
@@ -1,14 +1,71 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+//二叉搜索树
 #define Tnode sturct tnode
 typedef int Elemtype;
 
 struct tnode{
-		Elemtype k;
-		Elemtype count;
-		Tnode* lchild;
-		Tnode* rchild;
+		Elemtype data;
+		int count;
+		Tnode* Tleft;
+		Tnode* Tright;
 		Tnode* parent;
 }
+
+Tnode* CreateTree(); //建树   
+Tnode* insert(Tnode* root, DataType data);//插入节点   
+void InBTree(Tnode* root); //中序遍历   
+void PreBTree(Tnode* root); //先序遍历   
+void PostBTree(BTree root);//后序遍历   
+
+Tnode* CreateTree(){  
+    BTree root = NULL;  
+    ElemType temp = 0; 
+    int i=0,count=0;
+    scanf("%d", &count); 
+    if(count==0) return root;
+    scanf("%d", &temp);
+    newNode->data = tmp ;
+    newNode->count = 1;
+    newNode->Tleft = NULL;  
+    newNode->Tright = NULL;
+    for(i=1;i<count;i++){  
+        root = insert(root, temp);    
+        scanf("%d", &temp);    
+    }  
+      
+    return root;   
+} 
+
+Tnode* insert(BTree root, Elemtype data){  
+    Tnode* ptr = root;  
+    Tnode* tempNode;   
+    Tnode* newNode = (BTree)malloc(sizeof(struct BTree));   
+    //初始化元素
+    ewNode->data = data ;  
+    newNode->count = 1;
+    newNode->Tleft = NULL;  
+    newNode->Tright = NULL;  
+      
+    
+    while(ptr != NULL){  
+        tempNode = ptr;
+        ptr->count++;
+        if(data < ptr->data)  
+            ptr = ptr->Tleft;   
+        else  
+            ptr = ptr->Tright;   
+              
+    }   
+          
+     if(tempNode->data >= data){  
+        tempNode->Tleft =  newNode;   
+        }else{  
+           tempNode->Tright =  newNode;   
+        
+        }   
+      
+    return root;   
+} 
+
 
