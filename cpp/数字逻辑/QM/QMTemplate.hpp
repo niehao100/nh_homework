c4bf3676957d00417ee35c4f2609fb4cca4053cf
niehao <niehao100@gmail.com>
2015-03-20 23:55:12 +0800

    完成二进制转换部分，使用了vector

--- cpp/数字逻辑/QM/QMTemplate.hpp
+++ cpp/数字逻辑/QM/QMTemplate.hpp
@@ -5,7 +5,7 @@
 // 直接把QMTemplate.hpp当做普通头文件来使用就可以了。实际上hpp就是.cpp和.h的合并。
 #ifndef _QM2013011280_HPP_
 #define _QM2013011280_HPP_
-
+#define INF=0xffffffffffffffff;
 #include <string>
 #include <vector>
 class QM201301180
@@ -35,8 +35,11 @@ public:
 			const unsigned int* d, unsigned int d_count) 
 	{
 		std::string retval;
-		std::int tmp=0,i=0,j=0;
-		
+		std::vector<unsigned char> *bit=new std::vector<unsigned char>(m_count);
+		int tmp=0,i=0,j=0;
+		QM201301180::flag_max=0;
+		QM201301180::FindHighbit(bit[0],m[m_count-1]);
+		QM201301180::FindHighbit(bit[0],d[d_count-1]);
 		//
 		//
 		// 填入你自己的算法
@@ -45,8 +48,24 @@ public:
 		return retval;
 	}
 
-	// 自己添加的成员函数必须是static，这样才可以很好地配合QM函数工作。
+	static std::vector<int> FindHighbit(std::vector<unsigned char> &bit, const unsigned int m){
+		int tmp = 0x8000000000000000,flag=0;
+		for(;tmp!=0;tmp=(int)(tmp/2)){
+			if((m&tmp)!=0) 
+				flag++;
+			if(flag!=1)
+				bit.push_back((m&tmp)!=0);
+		}
+		if(flag>QM201301180::flag_max) flag==flag_max;
+	}
 
+	static std::vector<int> Rebit(std::vector<unsigned char> &bit, const unsigned int m){
+		int tmp = 0,i=0;
+		for(i=flag_max;i>-1;tmp=1<<i)
+			bit.push_back((m&tmp)!=0);
+	}
+	// 自己添加的成员函数必须是static，这样才可以很好地配合QM函数工作。
+	static int flag_max;
 };
 
 #endif
