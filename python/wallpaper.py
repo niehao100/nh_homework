1376a8bfad5f6a2b660ec6ffd1710ec10da33f66
Nie Hao <niehao100@gmail.com>
2020-07-22 11:25:12 +0800

    Update wallpaper.py

--- python/wallpaper.py
+++ python/wallpaper.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# -*- author:insun -*-
+# -*- author:neil -*-
 # python抓取bing主页所有背景图片
-import urllib,re,sys,os
+import requests
+from bs4 import BeautifulSoup
+import re,sys,os
 def get_bing_backphoto():
     if (os.path.exists('photos')== False):
         os.mkdir('photos')
     Path=sys.path[0]
     for i in range(0,1):
-        url = 'http://cn.bing.com/HPImageArchive.aspx?format=js&idx='+str(i)+'&n=1&nc=1361089515117&FORM=HYLH1'
-        html = urllib.urlopen(url).read()
+        url = 'https://cn.bing.com/'
+        html = requests.get(url)
         if html == 'null':
             print ('open & read bing error!')
             sys.exit(-1)
-        reg = re.compile('"url":"(.*?)","urlbase"',re.S)
-        text = re.findall(reg,html)
+        #reg = re.compile('"url":"(.*?)","urlbase"',re.S)
+        #text = re.findall(reg,html.__str__())
         #http://s.cn.bing.net/az/hprichbg/rb/LongJi_ZH-CN8658435963_1366x768.jpg
+        soup = BeautifulSoup(html.text,"html.parser")
+        text = soup.find_all(id='bgImgProgLoad')
         for imgurl in text:
-            right = imgurl.rindex('/')
-            name = imgurl.replace(imgurl[:right+1],'')
-            savepath = '/home/niehao/photos/'+ name
-            urllib.urlretrieve('http://cn.bing.com/'+imgurl, savepath)
+            imgurl = imgurl['data-ultra-definition-src']
+            imgurl = imgurl.replace("1920","3840")
+            imgurl = imgurl.replace("1080","2160")
+            name = imgurl[11:imgurl.find('&')]
+            print(name)
+            savepath = Path +'/photos/'+ name
+            pic = requests.get('http://cn.bing.com'+imgurl).content
+            with open(savepath,'wb') as f:
+                f.write(pic)
+                f.close()
             cmd="/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///"+savepath
             os.system(cmd)
             print(name + ' save success!')
-get_bing_backphoto()
+get_bing_backphoto()        
